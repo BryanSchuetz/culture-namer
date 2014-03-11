a7fa3bc73abb1526e484333f4d281381ab487ae7
bryan schuetz <bschuetz@gmail.com>
2014-03-11 16:06:06 -0400

    pointing contrib-watch at the github repo, because 0.5.3 has an unpatched bug—not cleaning up files to be reloaded

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,38 @@
 module.exports = function(grunt) {
+    //All configuration goes here
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    shell: {
+      //Shell onfiguration
+      jekyllBuild: {
+        command: 'jekyll build'
+      }
+    },
+    watch: {
+      //Watch configuration
+      options: {
+          interrupt: true,
+          atBegin: true,
+          livereload: true
+      },
+      files: [
+      'index.html',
+      '_includes/*.html',
+      '_layouts/*.html',
+      '_posts/*.md',
+      '_config.yml',
+      'index.html',
+      'assets/css/main.scss',
+      'assets/_sass/*.scss',
+      '_data/*.yml',
+      '*.md'
+      ],
+      tasks: ['shell:jekyllBuild']
+    }
+  });
+  //Where we tell Grunt we plan to use this plug-in
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  //Where we tell Grunt what to do when we type "grunt"
   grunt.registerTask('default', ['shell']);
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        shell: {
-            jekyllBuild: {
-                command: 'jekyll build'
-            }
-        },
-        watch: {
-            files: [
-            'index.html',
-            '_includes/*.html',
-            '_layouts/*.html',
-            '_posts/*.md',
-            '_config.yml',
-            'index.html',
-            'assets/css/main.scss',
-            'assets/_sass/*.scss',
-            '_data/*.yml',
-            '*.md'
-            ],
-            tasks: ['shell:jekyllBuild'],
-            options: {
-                interrupt: true,
-                atBegin: true,
-                livereload: true
-            }
-        }
-    });
 };
